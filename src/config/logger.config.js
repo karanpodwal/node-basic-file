f7b57a5af96500a9a91c4476d051e6c15e2caee2
Karanpodwal07 <karan.anncode@gmail.com>
2024-08-20 15:58:26 +0530

    Added basic node express functionalitities

---
+++ src/config/logger.config.js
@@ -0,0 +1,22 @@
+
+const { createLogger, format, transports } = require('winston');
+
+const { combine, timestamp, label, printf } = format;
+
+
+const cutomFormat = printf(({ level, message, timestamp,error }) => {
+    return `${timestamp} : ${level}: ${message} `;
+})
+
+const logger =  createLogger({
+    format:combine(
+        timestamp({format:'yyyy-MM-dd hh:mm:ss'}),
+        cutomFormat
+      ),
+      transports: [ 
+        new transports.Console(),
+        new transports.File({ filename: 'combined.log' })  
+    ],
+})
+
+module.exports = logger;
---
+++ src/controllers/index.js
@@ -0,0 +1,17 @@
+
+
+// app.get('/api/v1/blogs',(req,res)=>{
+   
+// });
+
+// app.post('/api/v1/blogs',(req,res)=>{
+   
+// });
+
+// app.put('/api/v1/blogs',(req,res)=>{
+   
+// });
+
+module.exports = {
+    infoController: require('./info-controller')
+}
